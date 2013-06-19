2647435550080407ce2076f61e8040bc9a920acc
Sam Pullara <spullara@yahoo.com>
2013-06-19 10:51:20 -0700

    now report to sunnylabs

--- src/main/java/teslalogger/DataLogger.java
+++ src/main/java/teslalogger/DataLogger.java
@@ -6,10 +6,9 @@ import com.fasterxml.jackson.databind.MappingJsonFactory;
 import com.sampullara.cli.Args;
 import com.sampullara.cli.Argument;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Properties;
 
 /**
@@ -17,11 +16,15 @@ import java.util.Properties;
  */
 public class DataLogger {
 
+  public static final byte[] LF = "\n".getBytes();
   @Argument(alias = "c", description = "Name of the authentication properties file", required = true)
   private static String config;
 
   @Argument(alias = "p", description = "Minutes between log entries")
   private static Integer period = 5;
+
+  @Argument(alias = "h", description = "Hosts to report metrics")
+  private static String[] hosts;
 
   public static void main(String[] args) throws Exception {
     Properties auth = new Properties();
@@ -44,27 +47,50 @@ public class DataLogger {
 
       @Override
       public boolean call(long vehicleId, JsonNode vehicle) {
-        final Writer log;
         try {
-          log = new OutputStreamWriter(new FileOutputStream("vehicle_" + vehicleId + ".log", true), "UTF-8");
-          JsonGenerator logentry = jf.createGenerator(log);
-          logentry.writeStartObject();
-          logentry.writeObjectField("vehicle", vehicle);
-          logentry.writeObjectField("state", connection.getResult("vehicles/" + vehicleId + "/command/vehicle_state"));
-          logentry.writeObjectField("charge", connection.getResult("vehicles/" + vehicleId + "/command/charge_state"));
-          logentry.writeObjectField("climate", connection.getResult("vehicles/" + vehicleId + "/command/climate_state"));
-          logentry.writeObjectField("drive", connection.getResult("vehicles/" + vehicleId + "/command/drive_state"));
-          logentry.writeObjectField("settings", connection.getResult("vehicles/" + vehicleId + "/command/gui_settings"));
-          logentry.writeEndObject();
-          logentry.flush();
-          log.write("\n");
+          byte[] bytes = createLogEntry(vehicleId, vehicle);
+          OutputStream log = new FileOutputStream("vehicle_" + vehicleId + ".log", true);
+          log.write(bytes);
+          log.write(LF);
           log.close();
+          if (hosts != null) {
+            for (String host : hosts) {
+              send(bytes, host);
+            }
+          }
           return true;
         } catch (Exception e) {
+          e.printStackTrace();
           return false;
         }
       }
+
+      private byte[] createLogEntry(long vehicleId, JsonNode vehicle) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        JsonGenerator logentry = jf.createGenerator(baos);
+        logentry.writeStartObject();
+        logentry.writeObjectField("vehicle", vehicle);
+        logentry.writeObjectField("state", connection.getResult("vehicles/" + vehicleId + "/command/vehicle_state"));
+        logentry.writeObjectField("charge", connection.getResult("vehicles/" + vehicleId + "/command/charge_state"));
+        logentry.writeObjectField("climate", connection.getResult("vehicles/" + vehicleId + "/command/climate_state"));
+        logentry.writeObjectField("drive", connection.getResult("vehicles/" + vehicleId + "/command/drive_state"));
+        logentry.writeObjectField("settings", connection.getResult("vehicles/" + vehicleId + "/command/gui_settings"));
+        logentry.writeEndObject();
+        logentry.flush();
+        return baos.toByteArray();
+      }
     });
+  }
+
+  private static void send(byte[] bytes, String host) throws IOException {
+    URL url = new URL("http://" + host + "/report/metrics?t=tsdb&h=discusstesla.com&p=vehicle");
+    HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
+    urlc.setDoOutput(true);
+    urlc.addRequestProperty("Content-Type", "application/json");
+    OutputStream urlout = urlc.getOutputStream();
+    urlout.write(bytes);
+    urlout.close();
+    System.out.println("Sent to " + url + " " + urlc.getResponseCode());
   }
 
 }
